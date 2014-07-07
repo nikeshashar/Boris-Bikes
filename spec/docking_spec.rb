027cb37b42cc3a2cf6d47530906fe6bf43bef1fa
Nikesh Ashar <nikeshashar@gmail.com>
2014-07-07 18:18:25 +0100

    Defining Full Method

--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -3,7 +3,7 @@ require 'docking'
 describe DockingStation do
 
 	let(:bike) { Bike.new }
-	let(:station) {DockingStation.new}
+	let(:station) {DockingStation.new(:capacity => 20)}
 	
 	it "should accept a bike" do
 		expect(station.bike_count).to eq(0)
@@ -11,10 +11,17 @@ describe DockingStation do
 		expect(station.bike_count).to eq (1)
 	end	
 
-	it 'should release a bike' do
+	it "should release a bike" do
 		station.dock(bike)
 		station.release(bike)
 		expect(station.bike_count).to eq(0)
 	end
 
+	it "should know when its full" do
+		expect(station).not_to be_full
+		20.times { station.dock(Bike.new) }
+		expect(station).to be_full	
+	end
+
+	
 end
