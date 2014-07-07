a9388e731ee9690b282a8c4eab0b70e3acf3c468
Nikesh Ashar <nikeshashar@gmail.com>
2014-07-07 16:38:25 +0100

    Making the broken method work properly

---
+++ spec/bike_spec.rb
@@ -0,0 +1,15 @@
+require 'bike'
+
+describe Bike do
+
+	it 'should not be broken after being made' do
+		the_bike = Bike.new
+		expect(the_bike).not_to be_broken
+	end
+
+	it 'should be able to break' do
+		bike = Bike.new
+		bike.break!
+		expect(bike).to be_broken
+	end
+end
